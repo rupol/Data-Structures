d97eaf9bab94957aa0aefb5251a70ec556271538
Ruth Poliakon <r.poliakon@gmail.com>
2020-06-18 19:39:20 -0600

    Data structures II lecture

--- src/part_1/linked_list.py
+++ src/part_1/linked_list.py
@@ -4,10 +4,24 @@ class Node:
         self.next_node = next_node
 
 # build a class to manage nodes
+
+
 class LinkedList:
     def __init__(self):
-        self.head = None # stores a node that is the begining of the list
-        self.tail = None # stores a node that is the end of the list
+        self.head = None  # stores a node that is the begining of the list
+        self.tail = None  # stores a node that is the end of the list
+
+    # return all values in the list a -> b -> c -> None
+    def __str__(self):
+        output = ''
+        # create a tracker node variable
+        current_node = self.head
+        # loop until it's none
+        while current_node is not None:
+            output += f'{current_node.value} -> '
+            # update the tracker node to the next node
+            current_node = current_node.next_node
+        return output
 
     def add_to_head(self, value):
         # create a node to add
@@ -32,6 +46,7 @@ class LinkedList:
         else:
             # point the node at the current tail to the new node
             self.tail.next_node = new_node
+            # set tail to the new node
             self.tail = new_node
 
     def remove_head(self):
@@ -47,6 +62,7 @@ class LinkedList:
         # otherwise we have more elements in the list
         head_value = self.head.value
         self.head = self.head.next_node
+        return head_value
 
     def contains(self, value):
         if self.head is None:
@@ -62,17 +78,21 @@ class LinkedList:
             current_node = current_node.next_node
         return False
 
+
 linked_list = LinkedList()
 
-linked_list.add_to_head(0)
+linked_list.add_to_tail(0)
 linked_list.add_to_tail(1)
-print(f'does our LL contain 0? {linked_list.contains(0)}')
-print(f'does our LL contain 1? {linked_list.contains(1)}')
-print(f'does our LL contain 2? {linked_list.contains(2)}')
+linked_list.add_to_tail(2)
+linked_list.add_to_tail(3)
+print(linked_list)
+# print(f'does our LL contain 0? {linked_list.contains(0)}')
+# print(f'does our LL contain 1? {linked_list.contains(1)}')
+# print(f'does our LL contain 2? {linked_list.contains(2)}')
 
-linked_list.add_to_head(2)
-print(f'the start of the list is {linked_list.head.value}')
-linked_list.add_to_head(5)
-print(f'the start of the list is {linked_list.head.value}')
-linked_list.remove_head()
-print(f'the start of the list is {linked_list.head.value}')
+# linked_list.add_to_head(2)
+# print(f'the start of the list is {linked_list.head.value}')
+# linked_list.add_to_head(5)
+# print(f'the start of the list is {linked_list.head.value}')
+# linked_list.remove_head()
+# print(f'the start of the list is {linked_list.head.value}')
